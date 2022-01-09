5d4e071500b090e45f5ad58b55078b1f1f0ce4d3
최근영 <choi_keunyoung@choegeun-yeongs-MacBook-Air.local>
2022-01-09 16:41:43 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/numble/daangnservice/DaangnServiceApplication.java
@@ -0,0 +1,13 @@
+package numble.daangnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangnServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/numble/daangnservice/DaangnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package numble.daangnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
